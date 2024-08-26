88f7b4b12257f176f83705de47282281c90ad5e3
Guillaume Girishya <143455392+GGirishya@users.noreply.github.com>
2024-08-26 02:44:13 +0000

    Traverse both linked lists

--- Day1/solution.cpp
+++ Day1/solution.cpp
@@ -5,3 +5,8 @@ public:
         ListNode* Head = new ListNode(0);
         ListNode* current = Head;
         int carry = 0;
+             // Traverse both linked lists
+        while (l1 != nullptr || l2 != nullptr) {
+            int x = (l1 != nullptr) ? l1->val : 0;
+            int y = (l2 != nullptr) ? l2->val : 0;
+            int sum = carry + x + y;
