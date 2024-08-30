d3a16b9eed276c374ccf51bfc9775420ba7552f6
Guillaume Girishya (G) <143455392+GGirishya@users.noreply.github.com>
2024-08-30 05:39:48 +0000

    use dynamic progamming

--- Day5/solution.cpp
+++ Day5/solution.cpp
@@ -4,5 +4,8 @@ public:
         int n = s.length();
         if (n == 0) return "";
 
-         
+         // use dynamic progamming
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        int start = 0, maxLength = 1;
+
 };
