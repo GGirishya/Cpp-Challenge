64db3367a120139347736ac615ae27b2243791ac
Guillaume Girishya (G) <143455392+GGirishya@users.noreply.github.com>
2024-08-30 05:40:32 +0000

    Single characters are palindromes

--- Day5/solution.cpp
+++ Day5/solution.cpp
@@ -7,5 +7,9 @@ public:
          // use dynamic progamming
         vector<vector<bool>> dp(n, vector<bool>(n, false));
         int start = 0, maxLength = 1;
+        // Single characters are palindromes
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
 
 };
