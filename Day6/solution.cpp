b8680283e29eec3077f43cc21f5668d5dbadfe28
Guillaume Girishya (G) <143455392+GGirishya@users.noreply.github.com>
2024-09-02 01:48:43 +0000

    If the number of rows is 1, return the original string

---
+++ Day6/solution.cpp
@@ -0,0 +1,9 @@
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        // If the number of rows is 1, return the original string
+        if (numRows == 1) return s;
+};
+
+
