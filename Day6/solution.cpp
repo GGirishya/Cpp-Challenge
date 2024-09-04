dcfca3b160adce8b97509ddb067e38a9be68f67a
Guillaume Girishya (G) <143455392+GGirishya@users.noreply.github.com>
2024-09-04 06:02:35 +0000

    Create a vector of strings for each row

--- Day6/solution.cpp
+++ Day6/solution.cpp
@@ -4,6 +4,13 @@ public:
     string convert(string s, int numRows) {
         // If the number of rows is 1, return the original string
         if (numRows == 1) return s;
+
+        // Create a vector of strings for each row
+        vector<string> rows(min(numRows, int(s.length())));
+        
+        int currentRow = 0;
+        bool goingDown = false;
+
 };
 
 
